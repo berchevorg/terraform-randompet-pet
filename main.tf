97d0a94b78cadfe42d15192f69700f44553fabb2
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2024-05-09 07:30:04 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,29 @@
+variable "length" {
+  type = string
+#   default = "5"
+}
+
 resource "random_pet" "name" {
  length    = "9"
  separator = "-"
 }
+
+ resource "random_pet" "name44" {
+  length    = var.length
+  separator = "-"
+ }
 
 
 resource "null_resource" "helloWorld" {
   provisioner "local-exec" {
     command = "echo ${random_pet.name.id}"
   }
+}
+
+output "random_pet_name" {
+  value       = random_pet.name.id
+}
+
+output "random_pet_name44" {
+  value       = random_pet.name44.id
 }
